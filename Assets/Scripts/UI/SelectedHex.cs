1ebb75050d829b6fd14036b7d08aaf0235944663
Thomas Schemmer <ThomasSchemmer@gmx.de>
2023-05-19 17:07:28 -0400

    small ui overhaul

--- Assets/Scripts/UI/SelectedHex.cs
+++ Assets/Scripts/UI/SelectedHex.cs
@@ -12,10 +12,11 @@ public class SelectedHex : MonoBehaviour
         Selector.OnItemDeSelected += Hide;
 
         Background = transform.GetChild(0).gameObject;
-        TileText = transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
-        BuildingTypeText = transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
-        ProductionText = transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>();
-        CorruptedText = transform.GetChild(4).gameObject.GetComponent<TextMeshProUGUI>();
+        LocationText = transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+        TileText = transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
+        BuildingTypeText = transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>();
+        ProductionText = transform.GetChild(4).gameObject.GetComponent<TextMeshProUGUI>();
+        CorruptedText = transform.GetChild(5).gameObject.GetComponent<TextMeshProUGUI>();
         Hide();
     }
 
@@ -70,9 +71,11 @@ public class SelectedHex : MonoBehaviour
 
     private void ShowTile(bool bShouldShow) {
         if (bShouldShow) {
-            TileText.text = SelectedHexTile.Type.ToString() + SelectedHexTile.Location.GlobalTileLocation.ToString();
+            LocationText.text = SelectedHexTile.Location.GlobalTileLocation.ToString();
+            TileText.text = SelectedHexTile.Type.ToString();
         } else {
             TileText.text = string.Empty;
+            LocationText.text = string.Empty;
         }
     }
 
@@ -120,12 +123,15 @@ public class SelectedHex : MonoBehaviour
         if (!bShouldShow)
             return;
 
+        bool bHasBuilding = MapGenerator.TryGetBuildingAt(SelectedHexTile.Location, out BuildingData Building);
+        UnitsUI.transform.localPosition = bHasBuilding ? OffsetUIUnit_OnBuilding : OffsetUIUnit;
+
         Workers.TryGetWorkersAt(SelectedHexTile.Location, out List<WorkerData> WorkersAtHex);
 
         for (int i = 0; i < WorkersAtHex.Count; i++) {
             WorkerData Worker = WorkersAtHex[i];
             GameObject Prefab = Instantiate(UnitPrefab, UnitsUI.transform);
-            Prefab.transform.localPosition = OffsetUIUnit + OffsetUIPerUnit * i;
+            Prefab.transform.localPosition = OffsetUIPerUnit * i;
             Prefab.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = Worker.Name;
         }
     }
@@ -170,13 +176,14 @@ public class SelectedHex : MonoBehaviour
 
     public SelectWorkerScreen SelectWorkerScreen;
    
-    private TextMeshProUGUI TileText, BuildingTypeText, ProductionText, CorruptedText;
+    private TextMeshProUGUI LocationText, TileText, BuildingTypeText, ProductionText, CorruptedText;
     private GameObject Background;
     private HexagonData SelectedHexTile;
 
     private static Vector3 OffsetUIWorker = new Vector3(-100, 0, 0);
     private static Vector3 OffsetUIPerWorker = new Vector3(75, 0, 0);
-    private static Vector3 OffsetUIUnit = new Vector3(0, 0, 0);
+    private static Vector3 OffsetUIUnit = new Vector3(0, 155, 0);
+    private static Vector3 OffsetUIUnit_OnBuilding = new Vector3(0, 200, 0);
     private static Vector3 OffsetUIPerUnit = new Vector3(0, 55, 0);
 
 }
